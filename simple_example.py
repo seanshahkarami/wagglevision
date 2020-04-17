ab46cc3541724de9c8f7634c2f202c1a6734f4af
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-04-17 15:54:30 -0500

    clean up

--- simple_example.py
+++ simple_example.py
@@ -2,8 +2,8 @@ from wagglevision.datasets import CloudDataset
 from wagglevision.models import fcn_resnet101
 
 # test datasets
-train_data = CloudDataset('data', image_set='train')
-val_data = CloudDataset('data', image_set='val')
+train_data = CloudDataset('data', image_set='train', download=True)
+val_data = CloudDataset('data', image_set='val', download=False)
 
 print(len(train_data))
 print(len(val_data))
