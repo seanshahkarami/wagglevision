371703e30ca992d1f5004fc0e489e07a0501d42e
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-04-17 15:38:40 -0500

    added model example

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 from wagglevision.datasets import CloudDataset
+from wagglevision.models import fcn_resnet101
 
+# test datasets
 train_data = CloudDataset('data', image_set='train')
 val_data = CloudDataset('data', image_set='val')
 
 print(len(train_data))
 print(len(val_data))
+
+# test models
+net = fcn_resnet101(pretrained=True, num_classes=2)
